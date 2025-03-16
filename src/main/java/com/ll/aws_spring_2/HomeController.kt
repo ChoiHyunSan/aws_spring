e3e124108d11a9ccf7fe205033f4aca904f260ed
ChoiHyunSan <chsan626@gmail.com>
2025-03-16 17:39:23 +0900

    test: 자동 배포가 되도록 수정

--- src/main/java/com/ll/aws_spring_2/HomeController.kt
+++ src/main/java/com/ll/aws_spring_2/HomeController.kt
@@ -1,16 +1,19 @@
 package com.ll.aws_spring_2
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import software.amazon.awssdk.services.s3.S3Client
 
 @RestController
 class HomeController(
-    private val s3Service : S3Service
+    private val s3Service : S3Service,
+    @Value("\${custom.secretWord}")
+    private val secretValue : String
 ) {
     @GetMapping("/")
     fun main(): String {
-        return "Hi"
+        return "Secret Value : $secretValue ";
     }
 
     @GetMapping("/buckets")
--- src/test/java/com/ll/aws_spring_2/AwsSpring2ApplicationTests.java
+++ src/test/java/com/ll/aws_spring_2/AwsSpring2ApplicationTests.java
@@ -2,7 +2,9 @@ package com.ll.aws_spring_2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class AwsSpring2ApplicationTests {
 
